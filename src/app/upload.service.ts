ff8be6d4d72e7a94aa454e1f2169118ec510254e
sid7631 <siddharth7631@gmail.com>
2019-10-09 01:27:58 +0530

    updated file upload service for real backend

--- src/app/upload.service.ts
+++ src/app/upload.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpRequest, HttpEventType, HttpResponse } from '@angular/common/http';
 import { Subject, Observable, Observer } from 'rxjs';
 
-const url = "demo"
+const url = "http://127.0.0.1:8000/api/"
 
 export interface ExampleTab {
   label: string;
@@ -36,35 +36,35 @@ export class UploadService {
     const status: { [key: string]: { progress: Observable<number> } } = {};
 
     files.forEach(file => {
-      /* Upload logic to a real server*/
-      // const formData: FormData = new FormData();
-      // formData.append('file',file,file.name);
+      // /* Upload logic to a real server*/
+      const formData: FormData = new FormData();
+      formData.append('file',file,file.name);
 
-      // const req = new HttpRequest('POST', url,formData,{reportProgress:true});
+      const req = new HttpRequest('POST', url,formData,{reportProgress:true});
 
-      // const progress = new Subject<number>();
+      const progress = new Subject<number>();
 
-      // this.http.request(req).subscribe(event => {
-      //   if(event.type===HttpEventType.UploadProgress) {
-      //     const percentageDone = Math.round(100*event.loaded/event.total);
-      //     progress.next(percentageDone);
-      //   } else if(event instanceof HttpResponse){
-      //     progress.complete();
-      //   }
-      // });
+      this.http.request(req).subscribe(event => {
+        if(event.type===HttpEventType.UploadProgress) {
+          const percentageDone = Math.round(100*event.loaded/event.total);
+          progress.next(percentageDone);
+        } else if(event instanceof HttpResponse){
+          progress.complete();
+        }
+      });
 
-      // status[file.name] = {
-      //   progress:progress.asObservable()
-      // };
+      status[file.name] = {
+        progress:progress.asObservable()
+      };
 
       /* Demo upload behavior*/
-      const progress = new Subject<number>();
-      let percentageDone = 0;
-      let timerId = setInterval(() => { percentageDone = percentageDone + 25; progress.next(percentageDone); console.log(percentageDone) }, 1000)
-      setTimeout(() => { clearInterval(timerId); progress.complete(); console.log(progress) }, 4000)
-      status[file.name] = {
-        progress: progress.asObservable()
-      }
+      // const progress = new Subject<number>();
+      // let percentageDone = 0;
+      // let timerId = setInterval(() => { percentageDone = percentageDone + 25; progress.next(percentageDone); console.log(percentageDone) }, 1000)
+      // setTimeout(() => { clearInterval(timerId); progress.complete(); console.log(progress) }, 4000)
+      // status[file.name] = {
+      //   progress: progress.asObservable()
+      // }
     })
     return status;
   }
